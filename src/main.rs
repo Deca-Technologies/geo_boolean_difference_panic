987ae100bafb955e5b32a1a0adcaf00e25618fc1
Cassie Pearson <cassie.negrich@thinkdeca.com>
2023-01-24 16:23:44 -0700

    fix: removes excess iterator
    Removes an iterator that existed previously to help trim down the size
    of the multipolygon to the smallest failing set.

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,7 @@ fn main() {
 
     // Does not fail on every single call, so run continuously until failure is hit
     for _i in 0..1_000 {
-        let failing_subjects = MultiPolygon(
-            subjects.clone().into_iter().collect::<Vec<Polygon>>(),
-        );
-
-        BooleanOps::difference(&failing_subjects, &clip);
+        BooleanOps::difference(&subjects, &clip);
     }
 }
 
